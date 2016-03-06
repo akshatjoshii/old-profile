065c238d6a668205b832c4176f7b18e96bdd161a
akshat <akshat2@gmail.com>
2016-03-06 16:47:52 +0530

    parse data synced

--- js/jquery.contact.js
+++ js/jquery.contact.js
@@ -2,6 +2,7 @@
 	"use strict";
 	
 jQuery(document).ready(function(){
+	Parse.initialize("ORfIbExdxSmGrRXKkgdwromLqlUjftvMfOHSMXDN", "T12iCcWzLOEkvxVs5wF6y9wZHmF9jWlg0cEHZcuS");
 	$('#cform').submit(function(){
 
 		var action = $(this).attr('action');
@@ -13,19 +14,44 @@ jQuery(document).ready(function(){
 			.before('<img src="images/ajax-loader.gif" class="contact-loader" />')
 			.attr('disabled','disabled');
 
-		$.post(action, {
-			name: $('#name').val(),
-			email: $('#email').val(),
-			comments: $('#comments').val(),
-		},
-			function(data){
-				document.getElementById('message').innerHTML = data;
-				$('#message').slideDown('slow');
-				$('#cform img.contact-loader').fadeOut('slow',function(){$(this).remove()});
-				$('#submit').removeAttr('disabled');
-				if(data.match('success') != null) $('#cform').slideUp('slow');
-			}
-		);
+		//$.post(action, {
+		//	name: $('#name').val(),
+		//	email: $('#email').val(),
+		//	comments: $('#comments').val()
+		//},
+		//	function(data){
+		//		document.getElementById('message').innerHTML = data;
+		//		$('#message').slideDown('slow');
+		//		$('#cform img.contact-loader').fadeOut('slow',function(){$(this).remove()});
+		//		$('#submit').removeAttr('disabled');
+		//		if(data.match('success') != null) $('#cform').slideUp('slow');
+		//	}
+		//);
+			var data = {
+				name: document.getElementById('name').value,
+				email: document.getElementById('email').value,
+				comments: document.getElementById('comments').value
+			};
+
+			// Run our Parse Cloud Code and
+			// pass our 'data' object to it
+			Parse.Cloud.run("sendEmail", data, {
+				success: function (object) {
+					document.getElementById('message').innerHTML = 'Email Sent !!';
+					$('#message').slideDown('slow');
+					$('#cform img.contact-loader').fadeOut('slow', function () {
+						$(this).remove()
+					});
+					$('#submit').removeAttr('disabled');
+					$('#cform')[0].reset();
+				},
+
+				error: function (object, error) {
+					console.log(error);
+					document.getElementById('message').innerHTML = 'There is some problem, please try again !!';
+				}
+
+			});
 
 		});
 
