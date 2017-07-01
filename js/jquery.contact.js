b8d9813943163df7d70372ece20d5ce0ffed3767
Akshat Joshi <akshat2@gmail.com>
2017-07-01 14:38:45 +0530

    contact form updated

--- js/jquery.contact.js
+++ js/jquery.contact.js
@@ -2,9 +2,11 @@
 	"use strict";
 	
 jQuery(document).ready(function(){
-	Parse.initialize("ORfIbExdxSmGrRXKkgdwromLqlUjftvMfOHSMXDN", "T12iCcWzLOEkvxVs5wF6y9wZHmF9jWlg0cEHZcuS");
-	$('#cform').submit(function(){
+	 
+	$('#cform').submit(function(e){
 
+		e.preventDefault();
+		var $form = $(this);
 		var action = $(this).attr('action');
 
 		$("#message").slideUp(750,function() {
@@ -14,44 +16,62 @@ jQuery(document).ready(function(){
 			.before('<img src="images/ajax-loader.gif" class="contact-loader" />')
 			.attr('disabled','disabled');
 
-		//$.post(action, {
-		//	name: $('#name').val(),
-		//	email: $('#email').val(),
-		//	comments: $('#comments').val()
-		//},
-		//	function(data){
-		//		document.getElementById('message').innerHTML = data;
-		//		$('#message').slideDown('slow');
-		//		$('#cform img.contact-loader').fadeOut('slow',function(){$(this).remove()});
-		//		$('#submit').removeAttr('disabled');
-		//		if(data.match('success') != null) $('#cform').slideUp('slow');
-		//	}
-		//);
+
 			var data = {
 				name: document.getElementById('name').value,
 				email: document.getElementById('email').value,
 				comments: document.getElementById('comments').value
 			};
 
-			// Run our Parse Cloud Code and
-			// pass our 'data' object to it
-			Parse.Cloud.run("sendEmail", data, {
-				success: function (object) {
-					document.getElementById('message').innerHTML = 'Email Sent !!';
-					$('#message').slideDown('slow');
-					$('#cform img.contact-loader').fadeOut('slow', function () {
-						$(this).remove()
-					});
-					$('#submit').removeAttr('disabled');
-					$('#cform')[0].reset();
-				},
+				//name = $.trim($form.find('input[name="name"]').val()),
 
-				error: function (object, error) {
-					console.log(error);
+			jQuery.ajax({
+				type: "POST",
+				url: "http://formspree.io/akshat2@gmail.com",
+				data: $form.serialize(),
+				dataType: "json",
+				success: function (data) {
+					document.getElementById('message').innerHTML = 'Email Sent !!';
+							$('#message').slideDown('slow');
+							$('#cform img.contact-loader').fadeOut('slow', function () {
+								$(this).remove()
+							});
+							$('#submit').removeAttr('disabled');
+							$('#cform')[0].reset();
+				},
+				error: function () {
 					document.getElementById('message').innerHTML = 'There is some problem, please try again !!';
 				}
+		});
 
-			});
+			// $.ajax({
+			// 	method: 'POST',
+			// 	url: 'http://formspree.io/akshat2@gmail.com/',
+			// 	dataType: 'json',
+			// 	data: data,
+			// 	headers: {
+			// 		'Accept': 'application/json',
+			// 		'Content-Type': 'application/x-www-form-urlencoded'
+			// 	},
+            //
+			// 	beforeSend: function() {
+			// 	},
+			// 	success: function(data) {
+			// 		document.getElementById('message').innerHTML = 'Email Sent !!';
+			// 		$('#message').slideDown('slow');
+			// 		$('#cform img.contact-loader').fadeOut('slow', function () {
+			// 			$(this).remove()
+			// 		});
+			// 		$('#submit').removeAttr('disabled');
+			// 		$('#cform')[0].reset();
+            //
+			// 	},
+			// 	error: function(err) {
+			// 		document.getElementById('message').innerHTML = 'There is some problem, please try again !!';
+			// 	}
+			// });
+
+			 
 
 		});
 
